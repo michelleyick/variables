91d60260fa4ebb5d90e38a973888595095dd037d
41213 <41213@PC004141.coll.lr.local>
2014-09-22 10:18:21 +0100

    Completed tasks class exercises (development exercises)

---
+++ Class exercise development exercises Q1.py
@@ -0,0 +1,15 @@
+#Michelle Yick
+#22-09-2014
+#Class exercises development exercises Q1
+
+number1=int(input("Enter number1"))
+number2=int(input("Enter number2"))
+answer=number1//number2
+print(answer)
+
+number1=int(input("Enter number1"))
+number2=int(input("Enter number2"))
+answer=number1%number2
+print(answer)
+
+
---
+++ Class exercises development exercises Q2.py
@@ -0,0 +1,14 @@
+#Michelle Yick
+#22-09-2014
+#Class exercises development exercises Q2
+
+#Program that will turn a temperature from Fahrenheit to degrees Centigrade
+
+Fahrenheit=int(input("Enter temperature in Fahrenheit"))
+answer=(Fahrenheit-32)*(5/9)
+print(answer)
+
+
+
+
+
